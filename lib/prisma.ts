216ded41e1881c37c664d16bef406c664c564cd5
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2023-11-17 20:55:25 +0000

    fixed build due to prisma global error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,13 +2,18 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
+// Check if 'global' object exists
+declare const global: {
+  prisma?: PrismaClient;
+};
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = global.prisma as PrismaClient;
 }
 
 export default prisma;
